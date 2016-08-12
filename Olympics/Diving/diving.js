f4db9faa5cddfabd17739fc41ae662e01d6162cd
WJohnson1 <williamisamazing1@gmail.com>
2016-08-12 10:35:04 -0400

    dmv

--- Olympics/Diving/diving.js
+++ Olympics/Diving/diving.js
@@ -117,79 +117,79 @@ function end(){
 		if (y_score === 58.5){
 			second_score = 10
 			console.log(second_score)
-			text(second_score,825,163)
+			text(second_score,850,153)
 		}
 		if (x_score < 5 && x_score > -5){
 			first_score = 10
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)
+			text(first_score,550,153)
+			text(avg_score,1150,153)
 		}
 		if ((x_score < 10 && x_score > 5) | (x_score > -10 && x_score < -5)){
 			first_score = 9
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)			
+			text(first_score,550,153)
+			text(avg_score,1150,153)			
 		}
 		if ((x_score < 20 && x_score > 10) | (x_score > -20 && x_score < -10)){
 			first_score = 8
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)
+			text(first_score,550,153)
+			text(avg_score,1150,153)
 		}
 		if ((x_score < 30 && x_score > 20) | (x_score > -30 && x_score < -20)){
 			first_score = 7
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)
+			text(first_score,550,153)
+			text(avg_score,1150,153)
 		}				
 		if ((x_score < 40 && x_score > 30) | (x_score > -40 && x_score < -30)){
 			first_score = 6
 			console.log(first_score)		
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)		
+			text(first_score,550,153)
+			text(avg_score,1150,153)		
 		}
 		if ((x_score < 55 && x_score > 40) | (x_score > -50 && x_score < -40)){
 			first_score = 5
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)
+			text(first_score,550,153)
+			text(avg_score,1150,153)
 		}
 		if ((x_score < 70 && x_score > 55) | (x_score > -70 && x_score < -55)){
 			first_score = 4
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)					
+			text(first_score,550,153)
+			text(avg_score,1150,153)					
 		}
 		if ((x_score < 85 && x_score > 70) | (x_score > -85 && x_score < -70)){
 			first_score = 3
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)					
+			text(first_score,550,153)
+			text(avg_score,1150,153)					
 		}
 		if ((x_score < 100 && x_score > 85) | (x_score > -100 && x_score < -85)){
 			first_score = 2
 			console.log(first_score)
 			avg_score = (first_score + second_score)/2
 			console.log(avg_score)
-			text(first_score,525,163)
-			text(avg_score,1125,163)					
+			text(first_score,550,153)
+			text(avg_score,1150,153)					
 		}
 		if ((x_score < 115 && x_score > 100) | (x_score > -115 && x_score < -100)){
 			first_score = 1
@@ -211,9 +211,9 @@ function end(){
 		console.log(second_score)
 		avg_score = 0	
 		console.log(avg_score)
-		text(first_score,525,163)
-		text(second_score,825,163)
-		text(avg_score,1125,163)
+		text(first_score,550,153)
+		text(second_score,850,153)
+		text(avg_score,1150,153)
 	}
 	if(player.collide(wall)){
 		player.velocity.x = 0
