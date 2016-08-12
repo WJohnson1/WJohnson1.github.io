7f6879a40ceb29f137bad5cfb78fab4dc40b6356
WJohnson1 <williamisamazing1@gmail.com>
2016-08-12 15:19:49 -0400

    s

--- Olympics/Swimming/swimming.js
+++ Olympics/Swimming/swimming.js
@@ -1542,3 +1542,12 @@ function dopponent6(){
     	}    	    	
     }	
 }
+$('body').toggleClass('loaded');
+$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
