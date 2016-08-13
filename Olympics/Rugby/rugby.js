fed85768a4e3220cc21d27d1284c5b0a63a8f18f
WJohnson1 <williamisamazing1@gmail.com>
2016-08-13 15:06:02 -0400

    l

--- Olympics/Rugby/rugby.js
+++ Olympics/Rugby/rugby.js
@@ -95,7 +95,7 @@ function draw(){
 	if (game === true && homescreen === false){
 		background(0,255,0)
 		textSize(40)
-		text(score, 250,100)
+		text("0", 250,100)
 		text("Get to the End Zone with the ball", 50,200)
 		textSize(25)	
 		text("Without the Defenders Getting You", 125,275)
@@ -117,7 +117,11 @@ function draw(){
 		text("Press Enter to Play Again",75,490)
 		if (keyCode === ENTER) {
 			background(0,255,0)
+			a.remove()
 			e.remove()
+			g.remove()
+			f.remove()
+			b.remove()
 			p1.remove()
 			p2.remove()
 			p3.remove()
@@ -387,27 +391,37 @@ function end(){
 	}
 	if (player.collide(goal2)) {
 		if (round1 === true){
-			score ++
+			b = createSprite(250,100,75,75)
+			b.shapeColor = color(0,255,0)
+			text("1", 250,100)
 			round1 = false
 			barrier1.remove()
 		}	
 		if (round2 === true){
-			score ++
+			f = createSprite(250,100,75,75)
+			f.shapeColor = color(0,255,0)
+			text("2", 250,100)
 			round2 = false
 			barrier2.remove()
 		}	
 		if (round3 === true){
-			score ++
+			g = createSprite(250,100,75,75)
+			g.shapeColor = color(0,255,0)	
+			text("3", 250,100)
 			round3 = false
 			barrier3.remove()
 		}	
 		if (round4 === true){
-			score ++
+			e = createSprite(250,100,75,75)
+			e.shapeColor = color(0,255,0)
+			text("4", 250,100)
 			round4 = false
 			barrier4.remove()
 		}
 		if (round5 === true && round4 === false){
-			score = 5
+			a = createSprite(250,100,75,75)
+			a.shapeColor = color(0,255,0)
+			text("5", 250,100)
 			round5 = false
 			youWin()
 		}	
@@ -415,8 +429,6 @@ function end(){
 }
 function youWin(){
 	score = 5
-	e = createSprite(250,100,75,75)
-	e.shapeColor = color(0,255,0)
 	game = false
 	text("You Win", 200,390)
 }
